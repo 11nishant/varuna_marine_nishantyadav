4947a1ea582d0a46dd41d582e2b14749d90eb95e
11nishant <135135922+11nishant@users.noreply.github.com>
2026-01-06 17:36:26 +0530

    Update start-backend.ps1

--- backend/start-backend.ps1
+++ backend/start-backend.ps1
@@ -1,4 +1,4 @@
-$env:DATABASE_URL="postgresql://postgres:nishant@localhost:5432/fueleu"
+$env:DATABASE_URL="postgresql://postgres:nishant@localhost:5432/fueleu"   #database hosted locally, create your database in postgresql, and change your credential in place of nishant and use db freely
 Write-Host "Starting Backend Server on http://localhost:3001" -ForegroundColor Green
 Write-Host "Database: $env:DATABASE_URL" -ForegroundColor Cyan
 npm run dev
